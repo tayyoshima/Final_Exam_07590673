9005c262d149032c667a69325d47fdd0b746ba29
tayyoshima <43286859+tayyoshima@users.noreply.github.com>
2019-11-17 12:13:37 +0700

    Update DatabaseHelper.java
    Edit table Username

--- app/src/main/java/com/example/finalexam07590673/DatabaseHelper.java
+++ app/src/main/java/com/example/finalexam07590673/DatabaseHelper.java
@@ -8,7 +8,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME="register.db";
     public static final String TABLE_NAME="registration";
     public static final String COL_2="Name";
-    public static final String COL_3="User";
+    public static final String COL_3="Username";
     public static final String COL_4="Password";
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -16,7 +16,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT,Name TEXT,User TEXT,Password TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT,Name TEXT,Username TEXT,Password TEXT)");
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
